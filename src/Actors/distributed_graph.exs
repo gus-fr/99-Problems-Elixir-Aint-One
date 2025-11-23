1551eff0d8994d9a1bdcb3c745860a45d7a2dd8f
gus <gus@gus.org>
2025-11-23 15:50:08 +0100

    fix

--- src/Actors/distributed_graph.exs
+++ src/Actors/distributed_graph.exs
@@ -69,7 +69,7 @@ defmodule Graph do
   end
 end
 
-g = Graph.new([1, 2, 3, 4, 5], [[1, 2], [1, 3], [2, 4], [2, 6], [3, 5], [3, 7], [7,1]])
+g = Graph.new([1, 2, 3, 4, 5, 6, 7], [[1, 2], [1, 3], [2, 4], [2, 6], [3, 5], [3, 7], [7,1]])
 Graph.ping(g, 1)
 Graph.ping(g, 5)
 IO.puts("foo")
