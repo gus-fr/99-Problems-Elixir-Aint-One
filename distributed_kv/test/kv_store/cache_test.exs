9c726e26d82071f136d326d3db4ba0da6f0d87c6
gus <gus@gus.org>
2025-11-30 18:42:41 +0100

    WIP

--- distributed_kv/test/kv_store/cache_test.exs
+++ distributed_kv/test/kv_store/cache_test.exs
@@ -7,4 +7,12 @@ defmodule KVStore.CacheTest do
     assert bob_pid != KVStore.Cache.server_process(cache, "alice")
     assert bob_pid == KVStore.Cache.server_process(cache, "bob")
   end
+
+  test "to-do operations" do
+    {:ok, cache} = KVStore.Cache.start()
+    alice = KVStore.Cache.server_process(cache, "alice")
+    KVStore.KeyValueStore.put(alice, ~D[2023-12-19], "value1")
+    entries = KVStore.KeyValueStore.get(alice, ~D[2023-12-19])
+    assert "value1" = entries
+  end
 end
