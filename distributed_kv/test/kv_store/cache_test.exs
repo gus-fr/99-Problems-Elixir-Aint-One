f2d35f8f2d49c880e67a60a5f7dc9acf76dcfb35
gus <gus@gus.org>
2025-11-30 19:41:10 +0100

    test

--- distributed_kv/test/kv_store/cache_test.exs
+++ distributed_kv/test/kv_store/cache_test.exs
@@ -2,15 +2,15 @@ defmodule KVStore.CacheTest do
   use ExUnit.Case
 
   test "server_process" do
-    {:ok, cache} = KVStore.Cache.start()
-    bob_pid = KVStore.Cache.server_process(cache, "bob")
-    assert bob_pid != KVStore.Cache.server_process(cache, "alice")
-    assert bob_pid == KVStore.Cache.server_process(cache, "bob")
+    KVStore.Cache.start()
+    bob_pid = KVStore.Cache.server_process("bob")
+    assert bob_pid != KVStore.Cache.server_process("alice")
+    assert bob_pid == KVStore.Cache.server_process("bob")
   end
 
   test "to-do operations" do
-    {:ok, cache} = KVStore.Cache.start()
-    alice = KVStore.Cache.server_process(cache, "alice")
+    KVStore.Cache.start()
+    alice = KVStore.Cache.server_process("alice")
     KVStore.KeyValueStore.put(alice, ~D[2023-12-19], "value1")
     entries = KVStore.KeyValueStore.get(alice, ~D[2023-12-19])
     assert "value1" = entries
