414a9af9f8b780423ef1e700babaa04b09c9796c
gus <gus@gus.org>
2025-12-04 21:23:31 +0100

    updqte

--- advent_of_code/lib/code/code.ex
+++ advent_of_code/lib/code/code.ex
@@ -1,5 +1,5 @@
 defmodule AdventOfCode.Code do
-  @file_name "input.txt"
+  @file_name "input/input_day1.txt"
   @size_lock 100
   @start_position 50
 
@@ -7,30 +7,27 @@ defmodule AdventOfCode.Code do
     File.stream!(@file_name)
     |> Stream.map(&String.trim/1)
     |> Stream.map(&decode_rotation/1)
-    |> Stream.scan(@start_position,&rotate/2)
+    |> Stream.scan(@start_position, &rotate/2)
     |> Stream.filter(&(&1 == 0))
-    |> Enum.reduce(0,fn _, acc-> acc+1 end)
+    |> Enum.reduce(0, fn _, acc -> acc + 1 end)
   end
-
-
-
 
   defp decode_rotation(input_rotation) do
     case String.first(input_rotation) do
-      "L" -> {:ok, -1*parse_number(input_rotation)}
+      "L" -> {:ok, -1 * parse_number(input_rotation)}
       "R" -> {:ok, parse_number(input_rotation)}
       _ -> :error
     end
   end
 
   defp parse_number(input_rotation) do
-    case Integer.parse(String.slice(input_rotation,1,String.length(input_rotation))) do
-      {number,_} -> number
+    case Integer.parse(String.slice(input_rotation, 1, String.length(input_rotation))) do
+      {number, _} -> number
       _ -> :error
     end
   end
 
-  defp rotate({:ok,code},accumulator) do
+  defp rotate({:ok, code}, accumulator) do
     Integer.mod(accumulator + code, @size_lock)
   end
 end
