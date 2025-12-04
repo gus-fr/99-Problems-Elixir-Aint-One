8924660dc277e358de97648d2bfad8bb5e897070
gus <gus@gus.org>
2025-12-04 21:55:42 +0100

    WIP

--- advent_of_code/lib/gift_shop/gift_shop.ex
+++ advent_of_code/lib/gift_shop/gift_shop.ex
@@ -4,4 +4,20 @@ defmodule AdventOfCode.GiftShop do
   https://adventofcode.com/2025/day/2
   usage AdventOfCode.GiftShop.main
   """
+
+  @file_name "input/input_day2.txt"
+
+
+  def main() do
+    File.read!(@file_name)
+    |> String.split(",")
+    |> Enum.map(&String.split(&1,"-"))
+
+
+
+  end
+
+  defp expand_range(first,last) do
+    first..last
+  end
 end
