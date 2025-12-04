06ff6ddd6384b5b0732e080ff2ae1fd4304826aa
gus <gus@gus.org>
2025-12-04 22:50:01 +0100

    wip

--- advent_of_code/lib/gift_shop/gift_shop.ex
+++ advent_of_code/lib/gift_shop/gift_shop.ex
@@ -12,7 +12,7 @@ defmodule AdventOfCode.GiftShop do
     |> String.split(",")
     |> Enum.map(&String.split(&1, "-"))
     |> Enum.map(&tuple_to_range/1)
-    |> Enum.map(&invalid_id_sum/1)
+    |> Enum.map(&invalid_id_twice/1)
     |> Enum.sum()
   end
 
@@ -27,7 +27,7 @@ defmodule AdventOfCode.GiftShop do
     |> Enum.sum()
   end
 
-  defp invalid_id?(int_id) do
+  defp invalid_id_twice?(int_id) do
     digits = floor(:math.log10(int_id)) + 1
     shift = 10 ** div(digits, 2)
 
