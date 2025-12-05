98d6625211b6e43cefcfe2f21ed17ecd9f1fcb87
gus <gus@gus.org>
2025-12-05 18:40:37 +0100

    init matrix

---
+++ advent_of_code/lib/04_printing/pinting.ex
@@ -0,0 +1,29 @@
+defmodule AdventOfCode.Printing do
+  @moduledoc """
+  code for day 4 of AOC 2025
+  https://adventofcode.com/2025/day/4
+  usage
+  """
+
+  @file_name "input/input_day4.txt"
+
+  def main() do
+    read_matrix(@file_name)
+  end
+
+  def read_matrix(file_name) do
+    File.stream!(file_name)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.graphemes/1)
+    |> Stream.map(&Enum.map(&1, fn x -> to_bool!(x) end))
+    |> Enum.to_list()
+  end
+
+  defp to_bool!(char) do
+    case char do
+      "@" -> 1
+      "." -> 0
+      _ -> raise("bad char input #{char}")
+    end
+  end
+end
