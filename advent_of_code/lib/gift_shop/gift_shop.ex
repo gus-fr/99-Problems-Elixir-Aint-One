e22e79ad8c64a23c4270f3a1fffef25f02f76f2e
gus <gus@gus.org>
2025-12-05 11:00:17 +0100

    format

--- advent_of_code/lib/gift_shop/gift_shop.ex
+++ advent_of_code/lib/gift_shop/gift_shop.ex
@@ -57,8 +57,8 @@ defmodule AdventOfCode.GiftShop do
     {pattern, rest} = Enum.split(list, pattern_size)
 
     cond do
-      length(rest) ==pattern_size -> pattern == rest
-      length(rest) ==0 -> false
+      length(rest) == pattern_size -> pattern == rest
+      length(rest) == 0 -> false
       pattern == Enum.take(rest, pattern_size) -> repeated_patten?(pattern_size, rest)
       true -> false
     end
