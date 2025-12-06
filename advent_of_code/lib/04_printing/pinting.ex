11d1cbfca9c401db8071bd0b0fe6f352e2d0b77f
gus <gus@gus.org>
2025-12-06 15:44:56 +0100

    refacto with kernels

--- advent_of_code/lib/04_printing/pinting.ex
+++ advent_of_code/lib/04_printing/pinting.ex
@@ -12,17 +12,16 @@ defmodule AdventOfCode.Printing do
     matrix = read_matrix(@file_name)
 
     convo2d(matrix, 1)
-    #|> Enum.filter(fn x -> x < 4 end)
-    #|> length
+    |> Enum.filter(fn x -> x < 4 end)
+    |> length
   end
 
-  # line_above == nil -> Stream.repeatedly(fn -> 0 end)
   def convo2d(matrix, window_size) do
     height = length(matrix) - 1
     width = length(List.first(matrix)) - 1
 
-    for i <- 0..height,
-        j <- 0..width,
+    for i <- window_size..(height - window_size),
+        j <- window_size..(width - window_size),
         Enum.at(Enum.at(matrix, i), j) == 1,
         do: apply_convolution_fn(matrix_slice(matrix, i, j, window_size))
   end
@@ -33,20 +32,33 @@ defmodule AdventOfCode.Printing do
 
   def matrix_prodsum(matrix1, matrix2) do
     Stream.zip(List.flatten(matrix1), List.flatten(matrix2))
-    |> Enum.reduce(0,fn {x, y}, acc -> acc + x * y end)
+    |> Enum.reduce(0, fn {x, y}, acc -> acc + x * y end)
   end
 
   defp matrix_slice(matrix, i, j, window_size) do
-    Enum.slice(matrix, max(i - window_size, 0)..(i + window_size))
-    |> Enum.map(&Enum.slice(&1, max(j - window_size, 0)..(j + window_size)))
+    Enum.slice(matrix, (i - window_size)..(i + window_size))
+    |> Enum.map(&Enum.slice(&1, (j - window_size)..(j + window_size)))
   end
 
-  def read_matrix(file_name) do
+  defp pad_matrix(matrix, window_size) do
+    row_pad = [List.duplicate(0, length(List.first(matrix)) + window_size * 2)]
+
+    col_pad = List.duplicate(0, window_size)
+
+    row_pad ++
+      for(
+        row <- matrix,
+        do: col_pad ++ row ++ col_pad
+      ) ++ row_pad
+  end
+
+  defp read_matrix(file_name) do
     File.stream!(file_name)
     |> Stream.map(&String.trim/1)
     |> Stream.map(&String.graphemes/1)
     |> Stream.map(&Enum.map(&1, fn x -> char_to_bool!(x) end))
     |> Enum.to_list()
+    |> pad_matrix(1)
   end
 
   defp char_to_bool!(char) do
