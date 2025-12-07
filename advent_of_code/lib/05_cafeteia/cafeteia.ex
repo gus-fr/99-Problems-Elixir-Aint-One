3fac42ab2ef9d28472811ec6750edb4cb0736538
gus <gus@gus.org>
2025-12-07 16:53:34 +0100

    WIP

--- advent_of_code/lib/05_cafeteia/cafeteia.ex
+++ advent_of_code/lib/05_cafeteia/cafeteia.ex
@@ -2,9 +2,9 @@ defmodule AdventOfCode.Cafeteria do
   @moduledoc """
   code for day 5 of AOC 2025
   https://adventofcode.com/2025/day/5
-
-
-
+  Usage
+  fresh_ingredient_from_list part 1
+  fresh_ingredient_from_range for part 2
   """
 
   @file_name "input/input_day5.txt"
@@ -24,6 +24,63 @@ defmodule AdventOfCode.Cafeteria do
 
   def fresh_ingredient_from_range do
     {ranges, _} = load_data()
+
+    Enum.sort(ranges)
+    |> dedup_ranges
+  end
+
+  defp dedup_ranges(sorted_ranges) do
+    Enum.map_reduce(sorted_ranges, nil, fn x, acc ->
+      {range_difference(acc, x), forward_range(acc, x)}
+    end)
+    |> elem(0)
+    |> Enum.sum()
+  end
+
+  defp range_difference(early_range, later_range) do
+    cond do
+      early_range == nil ->
+        range_size(later_range)
+
+      early_range.last < later_range.first ->
+        range_size(later_range)
+
+      early_range.last < later_range.last ->
+        range_size((early_range.last + 1)..later_range.last)
+
+      early_range.last >= later_range.last ->
+        0
+
+      true ->
+        raise(
+          "sth went wrong #{early_range.first}..#{early_range.last} : #{later_range.first}..#{later_range.last}"
+        )
+    end
+  end
+
+  defp forward_range(early_range, later_range) do
+    cond do
+      early_range == nil ->
+        later_range
+
+      early_range.last < later_range.first ->
+        later_range
+
+      early_range.last < later_range.last ->
+        (early_range.last + 1)..later_range.last
+
+      early_range.last >= later_range.last ->
+        early_range
+
+      true ->
+        raise(
+          "sth went wrong in acc #{early_range.first}..#{early_range.last} : #{later_range.first}..#{later_range.last}"
+        )
+    end
+  end
+
+  defp range_size(range) do
+    1 + range.last - range.first
   end
 
   defp load_data() do
