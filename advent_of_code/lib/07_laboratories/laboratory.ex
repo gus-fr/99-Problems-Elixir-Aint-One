27c59e369b0b1c8b6e1ab4dc3b4604ceee40062e
gus <gus@gus.org>
2025-12-08 22:52:19 +0100

    WIP

--- advent_of_code/lib/07_laboratories/laboratory.ex
+++ advent_of_code/lib/07_laboratories/laboratory.ex
@@ -8,8 +8,18 @@ defmodule AdventOfCode.Laboratory do
   @file_name "input/input_day7.txt"
 
   def main() do
-    input_stream = read_input_stream()
-    decode_start_index(Enum.take(input_stream, 1))
+    lines =
+      read_input_stream()
+      |> Stream.map(&decode_line/1)
+
+    test =
+      advance_beam(Enum.at(lines, 0), {[], 0}, Enum.at(lines, 1))
+      #|> elem(0)
+      #IO.inspect(test, charlists: :as_lists)
+  end
+
+  defp advance_all_beams() do
+
   end
 
   defp read_input_stream() do
@@ -19,7 +29,30 @@ defmodule AdventOfCode.Laboratory do
     |> Stream.map(&String.graphemes/1)
   end
 
-  defp decode_start_index(first_line) do
-    hd(first_line) |> Enum.find_index(fn x -> x == "S" end)
+  defp decode_line(stream_line) do
+    Stream.with_index(stream_line)
+    |> Stream.filter(fn {char, _} -> char != "." end)
+    |> Stream.map(&elem(&1, 1))
+    |> Enum.to_list()
+  end
+
+  defp advance_beam([beam_idx | []], {new_beams, splits}, spliter_idxs) do
+    if Enum.member?(spliter_idxs, beam_idx) do
+      {[beam_idx - 1, beam_idx + 1 | new_beams], splits + 1}
+    else
+      {[beam_idx | new_beams], splits}
+    end
+  end
+
+  defp advance_beam([beam_idx | other_beams], {new_beams, splits}, spliter_idxs) do
+    if Enum.member?(spliter_idxs, beam_idx) do
+      advance_beam(
+        other_beams,
+        {[beam_idx - 1, beam_idx + 1 | new_beams], splits + 1},
+        spliter_idxs
+      )
+    else
+      advance_beam(other_beams, {[beam_idx | new_beams], splits}, spliter_idxs)
+    end
   end
 end
