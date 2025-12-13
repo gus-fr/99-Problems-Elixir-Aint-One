cf800c6e1d713373008f50075c84e116fa9acb7c
gus <gus@gus.org>
2025-12-13 18:47:20 +0100

    bug

--- advent_of_code/lib/08_playground/playground.ex
+++ advent_of_code/lib/08_playground/playground.ex
@@ -11,27 +11,53 @@ defmodule AdventOfCode.Playground do
     load_data()
     |> distance_matrix()
     |> Enum.sort_by(&elem(&1, 2))
+    |> Enum.take(1000)
     |> Enum.reduce(%{}, &add_circuit/2)
-    |> Enum.to_list()
-    |> length()
+    |> Map.to_list()
+    |> Enum.map(fn x -> length(MapSet.to_list(elem(x, 1))) end)
+    |> Enum.sort(:desc)
+
+    |> Enum.take(3)
+    |> Enum.reduce(1,fn x,y -> x*y end)
 
     #    |> length()
   end
 
   defp add_circuit({p1, p2, distance}, circuits) do
-    new_map =
+    circuit_key_p1 =
       for key <- Map.keys(circuits),
-          MapSet.member?(Map.get(circuits, key), p1) or
-            MapSet.member?(Map.get(circuits, key), p2),
-          into: %{} do
-        {key, MapSet.put(MapSet.put(Map.get(circuits, key), p1), p2)}
-      end
+          MapSet.member?(Map.get(circuits, key), p1),
+          do: key
 
-    if Map.equal?(new_map, %{}) do
-      Map.put(circuits, distance, MapSet.new([p1, p2]))
-    else
-      Map.put(circuits, hd(Map.keys(new_map)), hd(Map.values(new_map)))
+    circuit_key_p2 =
+      for key <- Map.keys(circuits),
+          MapSet.member?(Map.get(circuits, key), p2),
+          do: key
+
+    cond do
+      length(circuit_key_p1) == 0 and length(circuit_key_p2) == 0 ->
+        Map.put(circuits, distance, MapSet.new([p1, p2]))
+
+      length(circuit_key_p1) > 0 and length(circuit_key_p2) == 0 ->
+        Map.update(circuits, hd(circuit_key_p1), nil,fn x -> MapSet.put(x, p1) end)
+
+      length(circuit_key_p1) ==0 and length(circuit_key_p2) >0 ->
+        Map.update(circuits, hd(circuit_key_p2),nil, fn x -> MapSet.put(x, p2) end)
+
+      true ->
+        merge(circuits, hd(circuit_key_p1), hd(circuit_key_p2))
     end
+  end
+
+  defp merge(circuits, circuit_key_p1, circuit_key_p2) do
+    c1 = Map.get(circuits, circuit_key_p1)
+    c2 = Map.get(circuits, circuit_key_p2)
+
+    Map.put(
+      Map.drop(circuits, [circuit_key_p1, circuit_key_p2]),
+      circuit_key_p1,
+      MapSet.union(c1, c2)
+    )
   end
 
   defp point_distance({x1, y1, z1}, {x2, y2, z2}) do
