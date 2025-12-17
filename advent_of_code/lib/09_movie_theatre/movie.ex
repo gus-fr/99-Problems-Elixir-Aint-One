832a0e4f0c4697b7219cc4593c244ee09b858dc6
gus <gus@gus.org>
2025-12-17 19:59:44 +0100

    wip

--- advent_of_code/lib/09_movie_theatre/movie.ex
+++ advent_of_code/lib/09_movie_theatre/movie.ex
@@ -15,31 +15,58 @@ defmodule AdventOfCode.Movie do
 
   def main_part2() do
     data = load_data()
-    contours = draw_contour(data)
-    {upper,lower}=def_bounds(contours)
 
+    contours =
+      draw_contour(data)
+      |> Enum.to_list()
 
-    # data |> areas(contours)
+    {upper, lower} = def_bounds(contours)
+
+    x_index =
+      contours |> Enum.reduce(%{}, &reduce_index_x(&1, &2, elem(lower, 1), elem(upper, 1)))
+
+    y_index =
+      contours |> Enum.reduce(%{}, &reduce_index_y(&1, &2, elem(lower, 0), elem(upper, 0)))
+
+    data
+    |> areas(x_index, y_index)
     # |> Enum.max()
   end
 
+  def reduce_index_x({x, y}, acc_map, min_y, max_y) do
+    Map.update(acc_map, x, {max_y, min_y}, fn {min_val, max_val} ->
+      {min(min_val, y), max(max_val, y)}
+    end)
+  end
+
+  def reduce_index_y({x, y}, acc_map, min_x, max_x) do
+    Map.update(acc_map, y, {max_x, min_x}, fn {min_val, max_val} ->
+      {min(min_val, x), max(max_val, x)}
+    end)
+  end
+
   defp def_bounds(contours) do
-    upper_x = Enum.to_list(contours)
-    |> Enum.map(&elem(&1,0))
-    |> Enum.reduce(0,&max/2)
+    upper_x =
+      contours
+      |> Enum.map(&elem(&1, 0))
+      |> Enum.reduce(0, &max/2)
 
-    lower_x = Enum.to_list(contours)
-    |> Enum.map(&elem(&1,0))
-    |> Enum.reduce(upper_x,&min/2)
+    lower_x =
+      contours
+      |> Enum.map(&elem(&1, 0))
+      |> Enum.reduce(upper_x, &min/2)
 
-    upper_y = Enum.to_list(contours)
-    |> Enum.map(&elem(&1,1))
-    |> Enum.reduce(0,&max/2)
+    upper_y =
+      contours
+      |> Enum.map(&elem(&1, 1))
+      |> Enum.reduce(0, &max/2)
 
-    lower_y = Enum.to_list(contours)
-    |> Enum.map(&elem(&1,1))
-    |> Enum.reduce(upper_y,&min/2)
-    {{upper_x,upper_y},{lower_x,lower_y}}
+    lower_y =
+      contours
+      |> Enum.map(&elem(&1, 1))
+      |> Enum.reduce(upper_y, &min/2)
+
+    {{upper_x, upper_y}, {lower_x, lower_y}}
   end
 
   defp load_data() do
@@ -75,26 +102,29 @@ defmodule AdventOfCode.Movie do
     end
   end
 
-  defp areas(points, contour) do
-    for p1 <- points, p2 <- points, filter_points(p1, p2, contour), do: area(p1, p2)
+  defp areas(points, x_index, y_index) do
+    for p1 <- points, p2 <- points, in_contour(p1, p2, x_index, y_index), do: area(p1, p2)
   end
 
-  defp filter_points({x1, y1}, {x2, y2}, contour) do
-    if x1 <= x2 do
-      true
-    else
-      if in_contour({x1, y2}, contour) and in_contour({x2, y1}, contour) do
-        true
-      end
+  defp in_contour({x1, y1}, {x2, y2}, x_index, y_index) do
+    {lower_y1, upper_y1} = Map.get(x_index, x1)
+    {lower_y2, upper_y2} = Map.get(x_index, x2)
+    {lower_x1, upper_x1} = Map.get(y_index, y1)
+    {lower_x2, upper_x2} = Map.get(y_index, y2)
 
-      false
+    cond do
+      y2 < lower_y1 -> false
+      y2 > upper_y1 -> false
+      x2 < lower_x1 -> false
+      x2 > upper_x1 -> false
+
+      y1 < lower_y2 -> false
+      y1 > upper_y2 -> false
+      x1 < lower_x2 -> false
+      x1 > upper_x2 -> false
+      true -> true
     end
   end
-
-  defp in_contour({x, y}, contour) do
-    #for p1 <- contour, p2 <- contour, point_in_line(point, p1, p2), do: true
-  end
-
 
   defp areas(points) do
     for p1 <- points, p2 <- points, elem(p1, 0) <= elem(p2, 0), do: area(p1, p2)
